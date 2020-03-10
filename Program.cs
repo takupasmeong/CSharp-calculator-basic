9a26ab56dc6f553d939b710f5111decf8b4ac02d
takupasmeong <abiyu.aronggear@students.amikom.ac.id>
2020-03-10 23:29:11 +0700

    program awal

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Title = "Program Kalkulator";
+
+        Console.Write("Input nilai a = ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Input nilai b = ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine();
+
+        Console.WriteLine("Hasil penjumlahan {0} + {1} = {2}", a, b, penjumlahan(a, b));
+        Console.WriteLine("Hasil pengurangan {0} - {1} = {2}", a, b, pengurangan(a, b));
+        Console.WriteLine("Hasil perkalian {0} * {1} = {2}", a, b, perkalian(a, b));
+        Console.WriteLine("Hasil pembagian {0} / {1} = {2}", a, b, pembagian(a, b));
+
+        Console.WriteLine("tekan sembarang key untuk keluar...");
+        Console.ReadKey();
+    }
+
+    static int penjumlahan(int a, int b)
+    {
+        return a + b;
+    }
+
+    static int pengurangan(int a, int b)
+    {
+        return a - b;
+    }
+
+    static int perkalian(int a, int b)
+    {
+        return a * b;
+    }
+
+    static int pembagian(int a, int b)
+    {
+        return a / b;
+    }
+}
