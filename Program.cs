c471a0b30159c13c332911a33214b0421d03e201
takupasmeong <abiyu.aronggear@students.amikom.ac.id>
2020-03-11 05:29:50 +0700

    penambahan opsi pemilihan operasi aritmatika

--- Program.cs
+++ Program.cs
@@ -6,21 +6,42 @@ class Program
     {
         Console.Title = "Program Kalkulator";
 
+        Console.WriteLine("Pilihan menu kalkulator : \n");
+        Console.WriteLine("1. penjumlahan\n2.pengurangan\n3.perkalian\n4.pembagian");
+        Console.WriteLine();
+        
+        Console.Write("input nomor menu [1..4] : ");
+        int pilih = int.Parse(Console.ReadLine());
+
         Console.Write("Input nilai a = ");
         int a = int.Parse(Console.ReadLine());
 
         Console.Write("Input nilai b = ");
         int b = Convert.ToInt32(Console.ReadLine());
 
+        switch(pilih)
+        {
+            case 1:
+                Console.WriteLine("Hasil penjumlahan {0} + {1} = {2}", a, b, penjumlahan(a, b));
+            break;
+            case 2:
+                Console.WriteLine("Hasil pengurangan {0} - {1} = {2}", a, b, pengurangan(a, b));
+            break;
+            case 3:
+                Console.WriteLine("Hasil perkalian {0} * {1} = {2}", a, b, perkalian(a, b));
+            break;
+            case 4:
+                Console.WriteLine("Hasil pembagian {0} / {1} = {2}", a, b, pembagian(a, b));
+            break;
+            default:
+                Console.WriteLine("opsi tidak tersedia..");
+                
+            break;
+        }
         Console.WriteLine();
-
-        Console.WriteLine("Hasil penjumlahan {0} + {1} = {2}", a, b, penjumlahan(a, b));
-        Console.WriteLine("Hasil pengurangan {0} - {1} = {2}", a, b, pengurangan(a, b));
-        Console.WriteLine("Hasil perkalian {0} * {1} = {2}", a, b, perkalian(a, b));
-        Console.WriteLine("Hasil pembagian {0} / {1} = {2}", a, b, pembagian(a, b));
-
         Console.WriteLine("tekan sembarang key untuk keluar...");
         Console.ReadKey();
+        
     }
 
     static int penjumlahan(int a, int b)
